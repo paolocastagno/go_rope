fccfe9c12fc0635530335e3552ddeb996e3508d6
Paolo Castagno <paolo.castagno@unito.it>
2024-10-04 10:14:11 +0200

    Update project layout

---
+++ pkg/server/forwardingConf.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"github.com/quic-go/quic-go"
+
+	"github.com/paolocastagno/go_rope/util"
+)
+
+/////////// Logic parsing //////////////
+
+var ForwardDecision func(*util.RoPEMessage, *map[string]quic.EarlyConnection, int64) bool
+var ForwardBlock func(*util.RoPEMessage, quic.Stream)
+var ForwardSetLastResponse func(*util.RoPEMessage)
+var ForwardSetLastResponseBlock func(*util.RoPEMessage, quic.Stream)
